1dc975def8adba2abfe1458e3225da7d4dc4b0fc
Anneliese Gerrard <anneliesegerrard@hotmail.com>
2020-11-24 12:18:02 +0000

    created all routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
+
+  resources :courses, only: [:show] do
+    resources :lectures, only: [:create, :new, :show]
+  end
+  resources :exercises, only: [:show] do
+    resources :submissions, only: [:create]
+  end
 end
