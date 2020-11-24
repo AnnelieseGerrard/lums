a5c0781e1c59f67d8b6f74ac0b743cf209a1471b
Gareth Carter <gareth.carter@outlook.com>
2020-11-24 14:19:26 +0000

    Removed some page paths from the route file that were generated by the generate controller template

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   get 'lectures/show'
   get 'lectures/new'
   get 'lectures/create'
-  get 'lectures/show'
-  get 'lectures/new'
   get 'courses/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
