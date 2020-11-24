5a663a5ece38cedaef51ea73b53befbed1808bdb
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-24 02:10:59 +0000

    Performed configuration for cloudinary and active storage. Also added 'video' and 'resources' attachment fields for Lecture model

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,6 +1,8 @@
 class Lecture < ApplicationRecord
   belongs_to :course
   has_one :exercise
+  has_one_attached :video
+  has_many_attached :resources
 
   validates :title, presence: true
   validates :course, presence: true
