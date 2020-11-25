deb383554f011c684d65c43aca64a7d2c650d12b
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-25 15:32:35 +0000

    Got the submission box functionality working

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,9 +2,8 @@ class SubmissionsController < ApplicationController
   def create 
     @submission = Submission.new(user_id: params[:user_id], exercise_id: params[:exercise_id])
     @submission.file.attach(io: File.open(params[:file].tempfile), filename:params[:file].original_filename)
-    pry
     @submission.save
-    redirect_to '/courses/58/lectures/119'
+    redirect_to course_lecture_path(params[:course_id], params[:lecture_id])
   end
 
   def submission_params
