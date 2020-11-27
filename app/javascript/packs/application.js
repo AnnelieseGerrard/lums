232a12a1921f89787770233c6236782097db0e15
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-27 11:15:31 +0000

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,9 +36,3 @@ document.addEventListener('turbolinks:load', () => {
   initLectureSorting();
 });
 
-Dropzone.options.submissionUpload = {
-  paramName: "file", // The name that will be used to transfer the file
-  maxFilesize: 20, // MB
-  timeout: 180000,
-  acceptedFiles: ".zip"
-};
