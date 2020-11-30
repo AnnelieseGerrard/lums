d68817f62bed531397d9e565140da0a5a321f7c8
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-30 19:26:17 +0000

    Promising results

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap"
+import "dropzone"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -31,8 +32,6 @@ import { descriptionTrigger } from '../components/home';
 
 //turbolinks import
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
   checkScroll();
   smoothScroll();
   navChange();
