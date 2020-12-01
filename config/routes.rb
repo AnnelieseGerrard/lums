c84d17792a2fd696c9340c20e9ca1cafc9b9944b
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-12-01 00:03:57 +0000

    Refactored the error404 route into an errors controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'errors/error_404'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
-  get "errors/404", to: "pages#error_404"
+  get "errors/404", to: "errors#error_404"
   
   resources :courses, only: [:index, :show] do
     resources :lectures, only: [:create, :new, :show, :edit, :update, :destroy] do 
