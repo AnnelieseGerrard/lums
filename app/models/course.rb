870b39b3154cadc4b94d0495f9f17b3f00883b61
Soel Labat <soel.labat@outlook.com>
2020-12-04 10:41:45 +0100

    added dependency to reviews in the course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :user
   has_many :enrollments, dependent: :destroy
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :lectures, -> { order(position: :asc) }, dependent: :destroy
   has_one_attached :image, dependent: :destroy
 
